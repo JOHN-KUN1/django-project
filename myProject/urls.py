6fe2966880e14f32a27633f29957de1e8de56990
john <elitejhn5@gmail.com>
2024-12-31 23:13:30 +0100

    changes in settings

--- myProject/urls.py
+++ myProject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path('blog/',include('blog.urls')),
     path('users/',include('users.urls')),
     path('weather/',include('weather.urls'))
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
