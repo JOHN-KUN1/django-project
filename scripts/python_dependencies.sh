134653b2d3001679d8fa9ee678405047044de167
john <elitejhn5@gmail.com>
2024-12-31 20:47:35 +0100

    changes in settings

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-virtualenv /home/ubuntu/env
-source /home/ubuntu/env/bin/activate
+virtualenv /home/ubuntu/.venv
+source /home/ubuntu/.venv/Scripts/activate
 pip install -r /home/ubuntu/myProject/requirements.txt
